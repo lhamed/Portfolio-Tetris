b16bf19fd5d0d77cbaec9c13375d71711b5cc786
정윤재 <>
2023-07-07 14:56:42 +0900

    add GameDataSet

--- GameDataSet.cs
+++ GameDataSet.cs
@@ -5,6 +5,35 @@ namespace Portfolio_Tetris
     /// </summary>
     public class GameDataSet
     {
-        
+        private int height;
+        private int width;
+        public FlyingBlock currentFlyingBlock;
+        public bool[,] fallenBlocks;
+
+        public GameDataSet(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+            fallenBlocks = new bool[height, width];
+        }
+
+        public void AddFlyingBlock(FlyingBlock block)
+        {
+            // 이미 있다면 나중에 처리해주자. 
+            if (currentFlyingBlock != null)
+            {
+                //TODO 
+            }
+            currentFlyingBlock = block;
+        }
+    }
+
+    /// <summary>
+    /// 현재 움직일 수 있는 (떨어지고 있는) 블록이다. 
+    /// </summary>
+    public class FlyingBlock
+    {
+        public int Height;
+        public bool[,] Shape;
     }
 }
