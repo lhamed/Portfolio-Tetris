6a09c83a35a01ecf13abec3b1bd0e71f787fab50
정윤재 <>
2023-07-07 15:04:09 +0900

    GamePorcesser 설계

--- GameProcesser.cs
+++ GameProcesser.cs
@@ -5,6 +5,34 @@ namespace Portfolio_Tetris
     /// </summary>
     public class GameProcesser
     {
-        
+        public GameDataSet Process(GameDataSet dataSet)
+        {
+            // 유저 인풋 처리 ( 블록 회전 )
+            var dataSetAfterInput = ProcessUserInput(dataSet);
+
+            // 게임 내 중력 처리 ( 한칸 떨어지게 )
+            var dataSetAfterGravity = ProcessGravity(dataSetAfterInput);
+
+            // 가로로 다 채운 블록을 사라지게 하는 처리 ( 블록 클리어 처리 )
+            var dataSetAfterClearBlock = ClearFullBlock(dataSetAfterGravity);
+
+            return dataSetAfterClearBlock;
+        }
+
+
+        GameDataSet ProcessUserInput(GameDataSet dataSet)
+        {
+            return null;
+        }
+
+        GameDataSet ProcessGravity(GameDataSet dataSet)
+        {
+            return null;
+        }
+
+        GameDataSet ClearFullBlock(GameDataSet dataSet)
+        {
+            return null;
+        }
     }
 }
